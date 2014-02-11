63927cde2815f043b7d173890077979e4e454393
M@ <mattdunlap+github@gmail.com>
2014-02-10 19:07:47 -0500

    made it more whitespace robust

--- hw4_test.py
+++ hw4_test.py
@@ -4,44 +4,54 @@
 Note: Do not run this directly, it is called by hw4.py.
 """
 
+import re
 from utilities import *
 
-
+diagPromptsValidationRegex = r"(First string\?)\s*(Second string\?)\s*(Height\?)\s*(\S?.*)"
 
 def diag(stdin):
     r"""Test diag.
 
-    >>> diag('- X 7')[:40]  # Does it print the correct prompts?
+    >>> diag('- X 7')[0]  # Does it print the correct prompts?
     'First string? \nSecond string? \nHeight? \n'
-    >>> diag('- X 7')[40:]  # Does it handle single characters?
+    >>> diag('- X 7')[1]  # Does it handle single characters?
     '------\nX-----\nXX----\nXXX---\nXXXX--\nXXXXX-\nXXXXXX'
-    >>> diag('Batman NaNa 6')[40:]  # Does it handle multi-character strings?
+    >>> diag('Batman NaNa 6')[1]  # Does it handle multi-character strings?
     'BatmanBatmanBatmanBatmanBatman\nNaNaBatmanBatmanBatmanBatman\nNaNaNaNaBatmanBatmanBatman\nNaNaNaNaNaNaBatmanBatman\nNaNaNaNaNaNaNaNaBatman\nNaNaNaNaNaNaNaNaNaNa'
-    >>> diag('\\ / 5')[40:]  # Does it handle backslashes?
+    >>> diag('\\ / 5')[1]  # Does it handle backslashes?
     '\\\\\\\\\n/\\\\\\\n//\\\\\n///\\\n////'
     """
-    return testRun("./diag", [], stdin)[1].strip()
+    out = testRun("./diag", [], stdin)[1].strip()
+    match = re.match(diagPromptsValidationRegex, out)
+    normalForm = "%s \n%s \n%s \n" % (match.group(1)
+                                      , match.group(2)
+                                      , match.group(3))
+    return (normalForm, match.group(4)) if match else (out, out)
     # (first, second, height) = stdin.split()
     # height = int(height)
     # return 'First string? \nSecond string? \nHeight? \n' + "\n".join([second*i + first*(height-i-1) for i in range(height)])
 
+mirrorPromptsValidationRegex = r"(Maximum value\?)\s*(\S?.*)"
+
 def mirror(n):
     r"""Test mirror.
 
-    >>> mirror(5)[:16]  # Does it print the correct prompt?
+    >>> mirror(5)[0]  # Does it print the correct prompt?
     'Maximum value? \n'
-    >>> mirror(5)[16:]  # Does it handle reasonable numbers?
+    >>> mirror(5)[1]  # Does it handle reasonable numbers?
     '012345|543210\n012345|543210\n012345|543210\n012345|543210'
-    >>> mirror(0)[16:]  # Does it handle zero?
+    >>> mirror(0)[1]  # Does it handle zero?
     '0|0\n0|0\n0|0\n0|0'
-    >>> mirror(11)[16:]  # Does it correctly not reverse the digits of numbers larger than 9?
+    >>> mirror(11)[1]  # Does it correctly not reverse the digits of numbers larger than 9?
     '01234567891011|11109876543210\n01234567891011|11109876543210\n01234567891011|11109876543210\n01234567891011|11109876543210'
-    >>> mirror(100)[16:]  # Does it handle less reasonable numbers?
+    >>> mirror(100)[1]  # Does it handle less reasonable numbers?
     '0123456789101112131415161718192021222324252627282930313233343536373839404142434445464748495051525354555657585960616263646566676869707172737475767778798081828384858687888990919293949596979899100|1009998979695949392919089888786858483828180797877767574737271706968676665646362616059585756555453525150494847464544434241403938373635343332313029282726252423222120191817161514131211109876543210\n0123456789101112131415161718192021222324252627282930313233343536373839404142434445464748495051525354555657585960616263646566676869707172737475767778798081828384858687888990919293949596979899100|1009998979695949392919089888786858483828180797877767574737271706968676665646362616059585756555453525150494847464544434241403938373635343332313029282726252423222120191817161514131211109876543210\n0123456789101112131415161718192021222324252627282930313233343536373839404142434445464748495051525354555657585960616263646566676869707172737475767778798081828384858687888990919293949596979899100|1009998979695949392919089888786858483828180797877767574737271706968676665646362616059585756555453525150494847464544434241403938373635343332313029282726252423222120191817161514131211109876543210\n0123456789101112131415161718192021222324252627282930313233343536373839404142434445464748495051525354555657585960616263646566676869707172737475767778798081828384858687888990919293949596979899100|1009998979695949392919089888786858483828180797877767574737271706968676665646362616059585756555453525150494847464544434241403938373635343332313029282726252423222120191817161514131211109876543210'
-    >>> mirror(-10)[16:]  # Does it handle negative numbers?
+    >>> mirror(-10)[1]  # Does it handle negative numbers?
     ''
     """
-    return testRun("./mirror", [], str(n))[1].strip()
+    out = testRun("./mirror", [], str(n))[1].strip()
+    match = re.match(mirrorPromptsValidationRegex, out)
+    return ("%s \n"%match.group(1), match.group(2)) if match else (out, out)
     # if n < 0:
     #     return ''
     # else:
